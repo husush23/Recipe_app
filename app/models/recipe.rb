10e08e9f48f0afc5eef5ac81ce3a4527486bc477
husush23 <husseinkadare40@gmail.com>
2023-09-11 19:08:17 +0300

    created recipe and recipe_food

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+class Recipe < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+    has_many :recipe_foods, class_name:'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
+    has_many :foods, through: :recipe_foods
+
+    validates :name, presence: true,
+    validates :description, presence: true
+    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates: :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+end
