0ded4150d35a13d1dc70daa23afd792d9f35c1d0
husush23 <husseinkadare40@gmail.com>
2023-09-12 11:26:37 +0300

    Removed blan line

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
   has_many :foods, through: :recipe_foods
 
-
   validates :name, presence: true, length: { maximum: 250 }
   validates :description, presence: true
   validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
