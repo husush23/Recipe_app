9c6692200ea07ef2fd8230e4ae52ba5dd7149b25
husush23 <husseinkadare40@gmail.com>
2023-09-12 11:24:58 +0300

    Fixed errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,11 @@
 class Recipe < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-    has_many :recipe_foods, class_name:'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
-    has_many :foods, through: :recipe_foods
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
+  has_many :foods, through: :recipe_foods
 
-    validates :name, presence: true,
-    validates :description, presence: true,
-    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 },
-    validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
