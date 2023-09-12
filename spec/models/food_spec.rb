25d910426dd9defecd640bd8fe16c3b71fb717a2
husush23 <husseinkadare40@gmail.com>
2023-09-12 15:59:14 +0300

    Food model unit tests added

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  let(:user) { User.create(name: 'John Doe') }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      food = Food.new(
+        name: 'Test Food',
+        measurement_unit: 'g',
+        user: user,
+        quantity: 100,
+        price: 5
+      )
+      expect(food).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      food = Food.new(
+        measurement_unit: 'g',
+        user: user,
+        quantity: 100,
+        price: 5
+      )
+      expect(food).not_to be_valid
+    end
+
+    it 'is not valid without a measurement_unit' do
+      food = Food.new(
+        name: 'Test Food',
+        user: user,
+        quantity: 100,
+        price: 5
+      )
+      expect(food).not_to be_valid
+    end
+
+    it 'is not valid without a user_id' do
+      food = Food.new(
+        name: 'Test Food',
+        measurement_unit: 'g',
+        quantity: 100,
+        price: 5
+      )
+      expect(food).not_to be_valid
+    end
+
+    it 'is not valid without a quantity' do
+      food = Food.new(
+        name: 'Test Food',
+        measurement_unit: 'g',
+        user: user,
+        price: 5
+      )
+      expect(food).not_to be_valid
+    end
+
+    it 'is not valid without a price' do
+      food = Food.new(
+        name: 'Test Food',
+        measurement_unit: 'g',
+        user: user,
+        quantity: 100
+      )
+      expect(food).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'has many recipe_foods' do
+      association = described_class.reflect_on_association(:recipe_foods)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'destroys associated recipe_foods when destroyed' do
+      food = Food.create(
+        name: 'Test Food',
+        measurement_unit: 'g',
+        user: user,
+        quantity: 100,
+        price: 5
+      )
+      recipe = Recipe.create(
+        name: 'Test Recipe',
+        description: 'A test recipe',
+        preparation_time: 30,
+        cooking_time: 45,
+        user: user
+      )
+      recipe_food = RecipeFood.create(food: food, recipe: recipe, quantity: 2)
+      expect { food.destroy }.to change { RecipeFood.count }.by(-1)
+    end
+  end
+end
