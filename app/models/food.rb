56828387f673302f62fdfe4361e82e35285f8af9
husush23 <husseinkadare40@gmail.com>
2023-09-12 11:15:16 +0300

    Fixed

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,16 +1,16 @@
-class Food
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-    has_many :recipe_food, dependent: :destroy
+class Food < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_food, dependent: :destroy
 
-    attribute :name, :string
-    attribute :measurement_unit, :string
-    attribute :user_id, :integer
-    attribute :quantity, :integer
-    attribute :price, :integer
+  attribute :name, :string
+  attribute :measurement_unit, :string
+  attribute :user_id, :integer
+  attribute :quantity, :integer
+  attribute :price, :integer
 
-    validates :name, presence: true
-    validates :measurement_unit, presence: true
-    validates :user_id, presence: true
-    validates :quantity, presence: true
-    validates :price, presence: true
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :user_id, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
     has_many :foods, through: :recipe_foods
 
     validates :name, presence: true,
-    validates :description, presence: true
-    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    validates: :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :description, presence: true,
+    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 },
+    validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
