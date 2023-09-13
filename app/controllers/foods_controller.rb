e1635d8abdbbca094b8c2e913c16427b8546b18d
husush23 <husseinkadare40@gmail.com>
2023-09-13 11:59:55 +0300

    Fixed erros

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,39 +1,38 @@
 class FoodsController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @foods = Food.all
-    end
-    def new
-        @food = Food.new
-    end
-  
-    def show; end
+  before_action :authenticate_user!
+  def index
+    @foods = Food.all
+  end
 
-    def create
-        @food = current_user.foods.build(food_params)
-    
-        if @food.save
-          redirect_to foods_path, notice: 'Food was successfully created.'
-        else
-          flash.now[:alert] = 'Failed to create a food item. Please check the form for errors.'
-          render :new
-        end
-      end
-      
-  
-    def destroy
-        @food = Food.find(params[:id])
-        if @food.destroy
-          redirect_to foods_path, notice: 'Food was successfully deleted.'
-        else
-          redirect_to foods_path, alert: 'Failed to delete the food item.'
-        end
-    end
+  def new
+    @food = Food.new
+  end
 
-    private
+  def show; end
 
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+  def create
+    @food = current_user.foods.build(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      flash.now[:alert] = 'Failed to create a food item. Please check the form for errors.'
+      render :new
     end
   end
-  
+
+  def destroy
+    @food = Food.find(params[:id])
+    if @food.destroy
+      redirect_to foods_path, notice: 'Food was successfully deleted.'
+    else
+      redirect_to foods_path, alert: 'Failed to delete the food item.'
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+  end
+end
