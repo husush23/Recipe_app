205527f23e1c780381b2a3b38d40661589c12c0f
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-13 14:30:44 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'recipes#public_recipes' as :public_recipes
+
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :new, :create, :destroy, :public_recipes]
+  resources :recipes, only: [:index, :new, :create, :destroy] do
+      resources :recipe_foods, only: [ :new, :create]
+  end
 end
