f5d08681f03f98a66e9a03ab154c6ad0eb4a248e
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-13 13:11:09 +0200

    Render recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,28 @@
 class RecipesController < ApplicationController
+    def index
+        @recipes = Recipe.where(user_id: current_user.id)
+    end
+
+    def new
+        @recipe = Recipe.new
+        @user = current_user
+    end
+
+    def create
+        @user = current_user
+        @recipe = @user.recipes.build(new_recipe_params)
+
+        if @recipe.save
+        redirect_to recipes_path, notice: 'Recipe successfully created'
+        else
+        flash.now[:error] = @recipe.errors.full_messages.to_sentence
+        render :new
+        end
+    end
+
+    private
+
+    def new_recipe_params
+        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+    end
 end
